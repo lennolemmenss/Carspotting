1955eabe720e3844ead57fc6c41370e02cc7a9d5
lennolemmenss <132935957+lennolemmenss@users.noreply.github.com>
2024-01-15 20:46:17 +0100

    admin

--- resources/views/profile/partials/admin-header.blade.php
+++ resources/views/profile/partials/admin-header.blade.php
@@ -1,6 +1,5 @@
 <header>
     <nav class="navbar-1">
         <a class="navbar-brand" href="{{ route('admin.home') }}">Home</a>
-        <a class="navbar-brand" href="#">About</a>
     </nav>
 </header>
